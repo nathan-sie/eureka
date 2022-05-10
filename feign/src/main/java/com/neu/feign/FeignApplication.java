5221df110d5a0c730538443c7b109d430203d55a
nate <630480183@qq.com>
2022-05-10 20:17:51 +0800

    v1.2

--- feign/src/main/java/com/neu/feign/FeignApplication.java
+++ feign/src/main/java/com/neu/feign/FeignApplication.java
@@ -2,6 +2,7 @@ package com.neu.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class FeignApplication {
 
     public static void main(String[] args) {
